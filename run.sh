c92d99c332cd0b9f1c9dbac675bd9949b1c8af15
Northword <northword@outlook.com>
2022-08-10 20:36:31 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+cd ./citeproc-js-server
+
+echo '[info] Initing git submodule...'
+git submodule update --init --recursive
+
+echo '[info] Installing dependency...'
+export NODE_ENV=submodule
+npm install
+
+echo '[info] Starting citeproc serve...'
+npm start &
+sleep 5s # 缓一缓让 citeproc 跑起来再执行下一步
+
+cd ..
+echo '[info] Geting preview...'
+python preview.py
+
+echo '[info] Kill citeproc serve'
+kill $(ps -ef | grep node | awk '{print $2}')
+
+echo '[info] DONE!'
