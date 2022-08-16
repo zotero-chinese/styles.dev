a313e427ce23076f6c4ba56dfe184ede0b6fe5ed
Northword <northword@outlook.com>
2022-08-16 14:00:01 +0800

    add search

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,11 +1,17 @@
 import { defineUserConfig } from "@vuepress/cli";
-import docsearchPlugin from "@vuepress/plugin-docsearch";
+import { searchPlugin } from "@vuepress/plugin-search";
 import theme from "./theme";
 
 export default defineUserConfig({
+  title: "Zotero Chinese Styles",
   dest: "dist",
   lang: "zh-CN",
   base: '/styles/',
   theme,
   shouldPrefetch: false,
+  plugins: [
+    searchPlugin({
+      maxSuggestions: 10
+    }),
+  ],
 });
